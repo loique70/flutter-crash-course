fcf3506eb2715bbe044af8352ec4949bb8a6ead3
loique70 <fogouangloic10@gmail.com>
2023-11-11 15:58:12 -0800

    Listview and builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crash_couse/square.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,29 +10,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List _post = ['post 1', 'post 2', 'post 3', 'post 4'];
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            backgroundColor: Colors.white,
-            body: Center(
-                child: Container(
-              height: 200,
-              width: 200,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey.shade500,
-                        offset: const Offset(-4.0, -4.0),
-                        blurRadius: 15.0,
-                        spreadRadius: 1.0),
-                    const BoxShadow(
-                        color: Colors.white,
-                        offset: Offset(-4.0, -4.0),
-                        blurRadius: 15.0,
-                        spreadRadius: 1.0)
-                  ]),
-            ))));
+          body: ListView.builder(
+            itemCount: _post.length,
+            itemBuilder: (context, index) {
+              return MySquare(
+                child: _post[index],
+              );
+            },
+          ),
+        ));
   }
 }
