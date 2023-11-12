0917b42866774a7384d073c50883d7c0a6c34137
loique70 <fogouangloic10@gmail.com>
2023-11-11 16:32:04 -0800

    Image widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,26 +12,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            backgroundColor: Colors.white,
-            body: Center(
-                child: Container(
-              height: 200,
-              width: 200,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey.shade500,
-                        offset: const Offset(-4.0, -4.0),
-                        blurRadius: 15.0,
-                        spreadRadius: 1.0),
-                    const BoxShadow(
-                        color: Colors.white,
-                        offset: Offset(-4.0, -4.0),
-                        blurRadius: 15.0,
-                        spreadRadius: 1.0)
-                  ]),
-            ))));
+          body: Center(
+              child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Container(
+              height: 500,
+              width: 300,
+              color: Colors.amber,
+              child: Image.asset(
+                'lib/images/image.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+          )),
+        ));
   }
 }
